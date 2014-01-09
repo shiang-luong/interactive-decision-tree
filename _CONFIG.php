c3830fa4b8ccb932c0f4769cbdb32cdede7e8491
Judson Mitchell <judsonmitchell@gmail.com>
2014-01-09 13:38:40 -0600

    Add index and config file

---
+++ _CONFIG.php
@@ -0,0 +1,9 @@
+<?php
+
+define('APP_NAME','Wheeler Expungement App ');
+define('TARGET_TREE','showTree.html?1374682207');
+define("DBHOST","localhost");
+define("DBUSERNAME","root");
+define("CC_DBPASSWD", "PASSWORD");
+define("CC_DATABASE_NAME","inter-tree");
+
