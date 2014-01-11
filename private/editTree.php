e7ebbb4d937f371533ac8b9d79cd3a4d2d33bbdd
Judson Mitchell <judsonmitchell@gmail.com>
2014-01-11 15:59:53 -0600

    Get rid of unncessary xml

--- private/editTree.php
+++ private/editTree.php
@@ -15,19 +15,20 @@ if( !empty( $revision ) ){
 
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Interactive Decision Tree - Editor</title>
-<link href="css/editor.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/editor.js"></script>
+<link href="../public/css/editor.css" rel="stylesheet" type="text/css" />
+<link href="../public/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
+<div class="container">
 <h1>Interactive Decision Tree - Editor</h1>
 <div id="debug"></div>
+
 <?php
 switch( $cmd ){
 	case 'edit-tree':
@@ -56,6 +57,9 @@ switch( $cmd ){
 		showList( $tree );
 }
 ?>
+</div>
+<script type="text/javascript" src="../public/js/jquery.min.js"></script>
+<script type="text/javascript" src="../public/js/editor.js"></script>
 </body>
 </html>
 <?php
